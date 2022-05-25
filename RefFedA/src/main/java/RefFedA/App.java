497ffe5b8f89ef31aecfd1b4e414b874d4662292
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2022-05-25 16:44:54 +0200

    send and receive interactions

--- RefFedA/src/main/java/RefFedA/App.java
+++ RefFedA/src/main/java/RefFedA/App.java
@@ -15,9 +15,15 @@ import de.fraunhofer.iosb.tc_lib.IVCT_LoggingFederateAmbassador;
 import de.fraunhofer.iosb.tc_lib.IVCT_NullFederateAmbassador;
 import de.fraunhofer.iosb.tc_lib.IVCT_RTIambassador;
 import hla.rti1516e.CallbackModel;
+import hla.rti1516e.InteractionClassHandle;
+import hla.rti1516e.OrderType;
+import hla.rti1516e.ParameterHandleValueMap;
 import hla.rti1516e.RTIambassador;
+import hla.rti1516e.ResignAction;
 import hla.rti1516e.RtiFactory;
 import hla.rti1516e.RtiFactoryFactory;
+import hla.rti1516e.TransportationTypeHandle;
+import hla.rti1516e.encoding.EncoderFactory;
 import hla.rti1516e.exceptions.AlreadyConnected;
 import hla.rti1516e.exceptions.CallNotAllowedFromWithinCallback;
 import hla.rti1516e.exceptions.ConnectionFailed;
@@ -25,16 +31,25 @@ import hla.rti1516e.exceptions.CouldNotCreateLogicalTimeFactory;
 import hla.rti1516e.exceptions.CouldNotOpenFDD;
 import hla.rti1516e.exceptions.ErrorReadingFDD;
 import hla.rti1516e.exceptions.FederateAlreadyExecutionMember;
+import hla.rti1516e.exceptions.FederateInternalError;
+import hla.rti1516e.exceptions.FederateIsExecutionMember;
 import hla.rti1516e.exceptions.FederateNameAlreadyInUse;
 import hla.rti1516e.exceptions.FederateNotExecutionMember;
+import hla.rti1516e.exceptions.FederateOwnsAttributes;
+import hla.rti1516e.exceptions.FederateServiceInvocationsAreBeingReportedViaMOM;
+import hla.rti1516e.exceptions.FederatesCurrentlyJoined;
 import hla.rti1516e.exceptions.FederationExecutionAlreadyExists;
 import hla.rti1516e.exceptions.FederationExecutionDoesNotExist;
 import hla.rti1516e.exceptions.InconsistentFDD;
 import hla.rti1516e.exceptions.InteractionClassNotDefined;
+import hla.rti1516e.exceptions.InteractionClassNotPublished;
+import hla.rti1516e.exceptions.InteractionParameterNotDefined;
 import hla.rti1516e.exceptions.InvalidInteractionClassHandle;
 import hla.rti1516e.exceptions.InvalidLocalSettingsDesignator;
+import hla.rti1516e.exceptions.InvalidResignAction;
 import hla.rti1516e.exceptions.NameNotFound;
 import hla.rti1516e.exceptions.NotConnected;
+import hla.rti1516e.exceptions.OwnershipAcquisitionPending;
 import hla.rti1516e.exceptions.RTIinternalError;
 import hla.rti1516e.exceptions.RestoreInProgress;
 import hla.rti1516e.exceptions.SaveInProgress;
@@ -42,23 +57,25 @@ import hla.rti1516e.exceptions.UnsupportedCallbackModel;
 
 public class App extends IVCT_NullFederateAmbassador{
 
-    private static String FEDERATION_NAME = "NETN-Test-Federation";
-    private static String FEDERATE_NAME = "RefFedA";
-    private static String FEDERATE_TYPE = "RefFed";
+    protected String FEDERATION_NAME = "NETN-Test-Federation";
+    protected String FEDERATE_NAME = "RefFedA";
+    protected String FEDERATE_TYPE = "RefFed";
     
     public static final org.slf4j.Logger log = LoggerFactory.getLogger(App.class);
     private RTIambassador rtiAmbassador;
     private IVCT_LoggingFederateAmbassador loggingFederateAmbassador;
     private Interaction aggregate;
+    private EncoderFactory encoderFactory;
 
     public App(Logger logger) throws RTIinternalError {
         super(logger);
         RtiFactory rtiFactory = RtiFactoryFactory.getRtiFactory();
         rtiAmbassador = new IVCT_RTIambassador (rtiFactory.getRtiAmbassador(), rtiFactory.getEncoderFactory(),log);
         loggingFederateAmbassador = new IVCT_LoggingFederateAmbassador(this, log);
+        encoderFactory = rtiFactory.getEncoderFactory();
     }
     
-    private void connectToRti() throws InconsistentFDD, ErrorReadingFDD, CouldNotOpenFDD, NotConnected, RTIinternalError, MalformedURLException, CouldNotCreateLogicalTimeFactory, FederationExecutionDoesNotExist, SaveInProgress, RestoreInProgress, FederateAlreadyExecutionMember, CallNotAllowedFromWithinCallback, ConnectionFailed, InvalidLocalSettingsDesignator, UnsupportedCallbackModel, AlreadyConnected, FederateNameAlreadyInUse {
+    public void connectToRti() throws InconsistentFDD, ErrorReadingFDD, CouldNotOpenFDD, NotConnected, RTIinternalError, MalformedURLException, CouldNotCreateLogicalTimeFactory, FederationExecutionDoesNotExist, SaveInProgress, RestoreInProgress, FederateAlreadyExecutionMember, CallNotAllowedFromWithinCallback, ConnectionFailed, InvalidLocalSettingsDesignator, UnsupportedCallbackModel, AlreadyConnected, FederateNameAlreadyInUse {
         File fddFile = new File ("foms/TS-NETN-v4.0.xml");
         ArrayList<URL> foms = new ArrayList<>();
         foms.add(new File("foms/RPR-FOM-v2.0/RPR-Base_v2.0.xml").toURI().toURL());
@@ -67,32 +84,65 @@ public class App extends IVCT_NullFederateAmbassador{
         foms.add(new File("foms/NETN-FOM-v4.0/NETN-MRM.xml").toURI().toURL());
         foms.add(new File("foms/TS-NETN-v4.0.xml").toURI().toURL());
 
-        rtiAmbassador.connect(loggingFederateAmbassador, CallbackModel.HLA_EVOKED);
+        rtiAmbassador.connect(loggingFederateAmbassador, CallbackModel.HLA_IMMEDIATE);
         try {
-            //rtiAmbassador.createFederationExecution(FEDERATION_NAME, fddFile.toURI().toURL());
             rtiAmbassador.createFederationExecution(FEDERATION_NAME, foms.toArray(new URL[foms.size()]));
         } catch (FederationExecutionAlreadyExists ignored) {
         }
-        //rtiAmbassador.joinFederationExecution(FEDERATE_NAME, FEDERATE_TYPE, FEDERATION_NAME, foms.toArray(new URL[foms.size()]));
         rtiAmbassador.joinFederationExecution(FEDERATE_NAME, FEDERATE_TYPE, FEDERATION_NAME);
     }
     
+    public void setupDeclarations() throws NameNotFound, InvalidInteractionClassHandle, FederateNotExecutionMember, NotConnected, RTIinternalError, InteractionClassNotDefined, SaveInProgress, RestoreInProgress, FederateServiceInvocationsAreBeingReportedViaMOM {
+        aggregate = new Interaction.AggregateBuild(rtiAmbassador)
+            .addRemoveSubunits(true)
+            .build();
+
+        aggregate.publish(rtiAmbassador);
+        aggregate.subscribe(rtiAmbassador);
+    }
+
+    public void sendInteraction() throws FederateNotExecutionMember, NotConnected, InteractionClassNotPublished, InteractionParameterNotDefined, InteractionClassNotDefined, SaveInProgress, RestoreInProgress, RTIinternalError {
+        aggregate.send(rtiAmbassador);
+    }
+
+    public void disconnectFromRti() throws FederateIsExecutionMember, CallNotAllowedFromWithinCallback, RTIinternalError, InvalidResignAction, OwnershipAcquisitionPending, FederateOwnsAttributes, FederateNotExecutionMember, NotConnected, FederationExecutionDoesNotExist {
+        rtiAmbassador.resignFederationExecution(ResignAction.DELETE_OBJECTS);
+        try {
+            rtiAmbassador.destroyFederationExecution(FEDERATION_NAME);
+        } catch (FederatesCurrentlyJoined ignored) {
+            log.trace("leave federation open for remaining federates");
+        }
+        rtiAmbassador.disconnect();
+    }
+
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws RTIinternalError, InconsistentFDD, ErrorReadingFDD, CouldNotOpenFDD, FederationExecutionAlreadyExists, NotConnected, MalformedURLException, CouldNotCreateLogicalTimeFactory, FederationExecutionDoesNotExist, SaveInProgress, RestoreInProgress, FederateAlreadyExecutionMember, CallNotAllowedFromWithinCallback, ConnectionFailed, InvalidLocalSettingsDesignator, UnsupportedCallbackModel, AlreadyConnected, FederateNameAlreadyInUse, NameNotFound, InvalidInteractionClassHandle, FederateNotExecutionMember, InteractionClassNotDefined {
+    public static void main(String[] args) throws RTIinternalError, InconsistentFDD, ErrorReadingFDD, CouldNotOpenFDD, FederationExecutionAlreadyExists, NotConnected, MalformedURLException, CouldNotCreateLogicalTimeFactory, FederationExecutionDoesNotExist, SaveInProgress, RestoreInProgress, FederateAlreadyExecutionMember, CallNotAllowedFromWithinCallback, ConnectionFailed, InvalidLocalSettingsDesignator, UnsupportedCallbackModel, AlreadyConnected, FederateNameAlreadyInUse, NameNotFound, InvalidInteractionClassHandle, FederateNotExecutionMember, InteractionClassNotDefined, FederateIsExecutionMember, InvalidResignAction, OwnershipAcquisitionPending, FederateOwnsAttributes, FederatesCurrentlyJoined, InteractionClassNotPublished, InteractionParameterNotDefined, FederateServiceInvocationsAreBeingReportedViaMOM {
+        int duration = 8000;
+        int interval = 800;
 
         log.info("staring NETN reference federate A");
         App fed = new App(log);
+
+        // get environment settings
+        String name = System.getenv("federate");
+        if (name != null) {
+            fed.FEDERATE_NAME = name;
+        }
+        String durationString = System.getenv("duration");
+        if (durationString != null) {
+            duration = Integer.parseInt(durationString);
+        }
+
         fed.connectToRti();
 
         fed.setupDeclarations();
 
         // TODO implement the test logic here
-        int duration = 8000;
-        int interval = 800;
         while (duration > 0) {
+            fed.sendInteraction();
             try {
                 Thread.sleep(interval);
             } catch (InterruptedException e) {
@@ -102,13 +152,14 @@ public class App extends IVCT_NullFederateAmbassador{
             log.info("working - remaining time {} ms", duration);
         }
 
+        fed.disconnectFromRti();
+
         log.info("terminate NETN reference federate A");
     }
 
-    private void setupDeclarations() throws NameNotFound, InvalidInteractionClassHandle, FederateNotExecutionMember, NotConnected, RTIinternalError, InteractionClassNotDefined, SaveInProgress, RestoreInProgress {
-        aggregate = new Interaction.InteractionBuilder(rtiAmbassador, "MRM_Interaction.Request.Aggregate")
-            .removeSubunits("RemoveSubunits")
-            .build();
-        rtiAmbassador.publishInteractionClass(aggregate.getClassHandle());
-    }
+        // 6.13
+        @Override
+        public void receiveInteraction(final InteractionClassHandle interactionClass, final ParameterHandleValueMap theParameters, final byte[] userSuppliedTag, final OrderType sentOrdering, final TransportationTypeHandle theTransport, final SupplementalReceiveInfo receiveInfo) throws FederateInternalError {
+            log.warn("receiveInteraction not implemented");
+        }
 }
--- RefFedA/src/main/java/RefFedA/Interaction.java
+++ RefFedA/src/main/java/RefFedA/Interaction.java
@@ -1,47 +1,89 @@
 package RefFedA;
 
 import hla.rti1516e.RTIambassador;
+import hla.rti1516e.RtiFactoryFactory;
+import hla.rti1516e.encoding.EncoderFactory;
 import hla.rti1516e.exceptions.FederateNotExecutionMember;
+import hla.rti1516e.exceptions.FederateServiceInvocationsAreBeingReportedViaMOM;
+import hla.rti1516e.exceptions.InteractionClassNotDefined;
+import hla.rti1516e.exceptions.InteractionClassNotPublished;
+import hla.rti1516e.exceptions.InteractionParameterNotDefined;
 import hla.rti1516e.exceptions.InvalidInteractionClassHandle;
 import hla.rti1516e.exceptions.NameNotFound;
 import hla.rti1516e.exceptions.NotConnected;
 import hla.rti1516e.exceptions.RTIinternalError;
+import hla.rti1516e.exceptions.RestoreInProgress;
+import hla.rti1516e.exceptions.SaveInProgress;
 import hla.rti1516e.InteractionClassHandle;
-import hla.rti1516e.ParameterHandle;
+import hla.rti1516e.ParameterHandleValueMap;
 
 
+/**
+ * Container class for RTI interactions
+ */
 public class Interaction {
-    private final InteractionClassHandle messageId;
-    private final ParameterHandle removeSubunits;
+
+    private final InteractionClassHandle classHandle;   public InteractionClassHandle getClassHandle() { return classHandle; }
+    private final ParameterHandleValueMap parameters;   public ParameterHandleValueMap getParameters() { return parameters; }
     
-    private Interaction(InteractionClassHandle messageId, ParameterHandle removeSubunits) {
-        this.messageId = messageId;
-        this.removeSubunits = removeSubunits;
+    private Interaction(InteractionClassHandle messageId, ParameterHandleValueMap parameters) {
+        this.classHandle = messageId;
+        this.parameters = parameters;
     }
 
-    public InteractionClassHandle getClassHandle() {
-        return messageId;
+    public void subscribe (RTIambassador rtiAambassador) throws FederateServiceInvocationsAreBeingReportedViaMOM, InteractionClassNotDefined, SaveInProgress, RestoreInProgress, FederateNotExecutionMember, NotConnected, RTIinternalError  {
+        rtiAambassador.subscribeInteractionClass(classHandle);
     }
 
-    
+    public void publish (RTIambassador rtiAambassador) throws InteractionClassNotDefined, SaveInProgress, RestoreInProgress, FederateNotExecutionMember, NotConnected, RTIinternalError {
+        rtiAambassador.publishInteractionClass(classHandle);
+    }
+
+    public void send (RTIambassador rtiAambassador) throws InteractionClassNotPublished, InteractionParameterNotDefined, InteractionClassNotDefined, SaveInProgress, RestoreInProgress, FederateNotExecutionMember, NotConnected, RTIinternalError {
+        rtiAambassador.sendInteraction(classHandle, parameters, null);
+    }
+
+    /**
+     * Generic Interaction Builder to create Interaction instances
+     */
     public static class InteractionBuilder {
-        private RTIambassador rtiAambassador;
-        private InteractionClassHandle messageId;
-        private ParameterHandle removeSubunits;
+
+        protected RTIambassador rtiAambassador;
+        protected InteractionClassHandle messageId;
+        protected ParameterHandleValueMap parameters;
+        protected EncoderFactory encoderFactory;
 
         public InteractionBuilder (RTIambassador rtiAmbassador, String messageName) throws NameNotFound, FederateNotExecutionMember, NotConnected, RTIinternalError {
             this.rtiAambassador = rtiAmbassador;
             this.messageId = rtiAmbassador.getInteractionClassHandle(messageName);
+            parameters = rtiAmbassador.getParameterHandleValueMapFactory().create(1);
+            encoderFactory = RtiFactoryFactory.getRtiFactory().getEncoderFactory();
         }
         
-        public InteractionBuilder removeSubunits (String theName) throws NameNotFound, InvalidInteractionClassHandle, FederateNotExecutionMember, NotConnected, RTIinternalError {
-            this.removeSubunits = rtiAambassador.getParameterHandle(this.messageId, theName);
+        public InteractionBuilder addParameter (String parameterName, byte[] value) throws NameNotFound, InvalidInteractionClassHandle, FederateNotExecutionMember, NotConnected, RTIinternalError {
+            parameters.put(this.rtiAambassador.getParameterHandle(this.messageId, parameterName), value);
             return this;
         }
-        
+
         public Interaction build() {
-            Interaction aggregate = new Interaction(messageId, removeSubunits);
+            Interaction aggregate = new Interaction(messageId, parameters);
             return aggregate;
+        }
+    }
+
+    /**
+     * Model specific  builder for Aggregate interactions
+     */
+    public static class AggregateBuild extends InteractionBuilder {
+
+        public AggregateBuild(RTIambassador rtiAmbassador)
+                throws NameNotFound, FederateNotExecutionMember, NotConnected, RTIinternalError {
+            super(rtiAmbassador, "MRM_Interaction.Request.Aggregate");
+        }
+
+        public AggregateBuild addRemoveSubunits(boolean b) throws NameNotFound, InvalidInteractionClassHandle, FederateNotExecutionMember, NotConnected, RTIinternalError {
+            this.addParameter("RemoveSubunits",  encoderFactory.createHLAboolean(b).toByteArray());
+            return this;
         }
     }
 
